82571efa6ee4ab31e2d1f297f0f9ccbc082b498c
Filipe de Oliveira de Ataíde <filipe.ataide@lais.huol.ufrn.br>
2022-12-13 09:39:50 -0300

    Transicao

--- script.js
+++ script.js
@@ -96,22 +96,22 @@ let botoes = [
         texto: 'Combate às alterações climáticas',
         icone: 'visibility',
         cor: '#3E7E44',
-        top:'240px',
-        right:0
+        top: '240px',
+        right: 0
     },
     {
         texto: 'Vida debaixo d’água',
         icone: 'waves',
         cor: '#0397D9',
-        top:'320px',
-        right:'150px',
+        top: '320px',
+        right: '150px',
     },
     {
         texto: 'Vida sobre a terra',
         icone: 'nature',
         cor: '#55C02B',
-        top:'450px',
-        right:0, 
+        top: '450px',
+        right: 0,
     },
     {
         texto: 'Paz, justiça e instituições fortes',
@@ -129,6 +129,8 @@ let botoes = [
     }
 ]
 
+var main_title = $('.main-title');
+
 function addLeadingZeros(num) {
     if (num < 10) {
         return String(num).padStart(2, '0');
@@ -138,17 +140,113 @@ function addLeadingZeros(num) {
 
 botoes.forEach(
     function (value, index) {
-        console.log(value, index);
-        $('.buttons').append(
-            "<button class='btn rounded-circle' style='border: 2px solid "
-            + value.cor + "; color:" + value.cor + "; left:" + value.left +"; top:"+ value.top +"; right:"+ value.right +"'>" +
+        $('.buttons-bg').append(
+            "<button class='btn btn-bg btn-bg-" + index + " rounded-circle position-absolute' value='" + index + "' >" +
             addLeadingZeros(index + 1) + ".<br>" + value.texto + "<br>" +
             "<span class='material-symbols-outlined'>"
             + value.icone + "</span>" + "</button>"
         )
+
+        $('style').append(
+            ".btn-bg-" + index + ", .btn-mini-" + index + " {\n" +
+            "   color: " + value.cor + " !important;\n" +
+            "   border:  2px solid" + value.cor + " !important;\n" +
+            "}\n" +
+            ".btn-bg-" + index + " {\n" +
+            "   top: " + value.top + ";\n" +
+            "   left: " + value.left + ";\n" +
+            "   right: " + value.right + ";\n" +
+            "}\n" +
+            ".btn-bg-" + index + ":hover , .btn-mini-" + index + ":hover {\n" +
+            "   background-color: " + value.cor + "80 ;\n" +
+            "}\n" +
+            ".btn-sel-" + index + " {\n" +
+            "   background-color: " + value.cor + ";\n" +
+            "   color: white;" +
+            "}\n"
+        )
+
+        $('style').append(
+            ".button-title-" + index + ", .btn-mini-" + index + " {\n" +
+            "   color: " + value.cor + " !important;\n" +
+            "}\n" +
+            ".button-title-" + index + " .logo, " +
+            ".button-title-" + index + " .hr, " +
+            ".button-title-" + index + " .projetos{\n" +
+            "   border-color: " + value.cor + ";\n" +
+            "}\n" +
+            ".button-title:has(.button-title-" + index + ")::before{\n" +
+            "background: repeating-conic-gradient("+ value.cor + " 0deg 18deg, transparent 18deg 21.17647058823529deg);"
+            + "}\n"
+        )
     }
 );
 
+$('body').on('click', '.btn-bg, .btn-mini', function () {
+    $('.buttons-mini-col, .button-selected').html('');
+    var cls = '.buttons-mini-l';
+    var i = $(this).val();
+    botoes.forEach(
+        function (value, index) {
+            if (cls == '.buttons-mini-l' & $('.btn-mini').length == 8) {
+                cls = '.buttons-mini-r';
+            }
+            if (i != index) {
+                $(cls).append(
+                    "<button class='btn btn-mini btn-mini-" + index +
+                    " rounded-circle' value='" + index + "'><span class='material-symbols-outlined'>"
+                    + value.icone + "</span>" + "</button>"
+                )
+            }
+            else {
+                $('.button-selected').append(
+                    "<button class='btn btn-bg btn-sel-" + index + " rounded-circle' value='" + index + "'>" +
+                    addLeadingZeros(index + 1) + ".<br>" + value.texto + "<br>" +
+                    "<span class='material-symbols-outlined'>"
+                    + value.icone + "</span>" + "</button>"
+                )
+                $('.button-title').css('border-color', value.cor);
+                $('.button-title').html(
+                    "<div class='text button-title-" + index + "'><div class='p'><h3>" + addLeadingZeros(index + 1) + '. ' + value.texto + "</h3><p>" +
+                            `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore
+                            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                            ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.
+                            `
+                            +
+                            `</p><div class='hr my-3'></div><h3>Nossos projetos</h3>
+                        <div class="d-grid my-4">
+                            <div class="logo">
+                                <img  class="img-fluid" src="v1_logo-lais-azul-1.svg" alt="">
+                            </div>
+                            <div class="logo">
+                                <img class="img-fluid" src="ufrn-logo.png" alt="">
+                            </div>
+                        </div>
+                        <div class="projetos my-3" >
+        
+                        </div>
+                    </div>
+                </div>
+                `
+                )
+                //$('.button-title').append(
+
+                //)
+            }
+        }
+    );
+    $('.buttons').hide();
+    $('.buttons-mini').show();
+})
+
+$('body').on('click', '.button-voltar', function () {
+    $('.buttons').show();
+    $('.buttons-mini').hide();
+})
 
 var len = 100 / cores.length
 var grad = ''
